43bf6bc02d8f36d836f5a8248075857451452bb7
shockliang <shockwater@gmail.com>
2019-10-14 00:17:19 +0800

    Mongoose queries.

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,9 +13,12 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(`access token:${accessToken}`);
-      console.log(`proflie: ${profile}`);
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
