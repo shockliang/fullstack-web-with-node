3df0b585a5eac1808f6ac8c26405bb9c0254f916
shockliang <shockwater@gmail.com>
2019-10-15 00:13:48 +0800

    Passport callbacks.

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,12 +13,20 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-        } else {
-          new User({ googleId: profile.id }).save();
-        }
-      });
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            console.log("find one user and passport done");
+            done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   )
 );
