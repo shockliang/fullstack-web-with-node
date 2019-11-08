1d9c5c8a2b2f8231b94d53fb58d450eeba8d220c
shockliang <shockwater@gmail.com>
2019-11-09 00:59:30 +0800

    Route-specific middlewares.

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,8 +1,9 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 5 * 100,
       currency: "usd",
@@ -10,7 +11,7 @@ module.exports = app => {
       source: req.body.id
     });
 
-    req.user.credit += 5;
+    req.user.credits += 5;
     const user = await req.user.save();
 
     res.send(user);
