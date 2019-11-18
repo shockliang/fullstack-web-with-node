7aa60214905e5fcce116c61a65414cb99055ac53
shockliang <shockwater@gmail.com>
2019-11-19 00:10:52 +0800

    Creating recipients sub collection.

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -9,8 +9,11 @@ module.exports = app => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
+      subject,
       body,
-      subject
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSend: Date.now()
     });
   });
 };
