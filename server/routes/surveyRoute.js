516ed3354065c2f707397f366364a7511377f203
shockliang <shockwater@gmail.com>
2019-11-30 17:36:57 +0800

    Polish in the route handler.

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -7,7 +7,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTempalte");
 const Survey = mongoose.model("survey");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
@@ -19,5 +19,16 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (e) {
+      res.status(422).send(e);
+    }
   });
 };
