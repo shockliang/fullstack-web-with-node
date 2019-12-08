226bc076804ae83d840d4704518fa12f49de04ed
shockliang <shockwater@gmail.com>
2019-12-08 14:38:54 +0800

    DRY'ing up fields.

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -3,9 +3,10 @@
 
 import React from "react";
 
-export default ({ input }) => {
+export default ({ input, label }) => {
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
     </div>
   );
