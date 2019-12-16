4a817005dd3eac046fbfb1d4a7e9c9d1a0f82b90
shockliang <shockwater@gmail.com>
2019-12-16 20:56:13 +0800

    Refactoring form fields.

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
@@ -14,7 +14,7 @@ const SurveyFormReview = ({ onCancel }) => {
 
 function mapStateToProps(state) {
   return {
-    formValues: state.form.surveyForm.valuse
+    formValues: state.form.surveyForm.values
   };
 }
 
