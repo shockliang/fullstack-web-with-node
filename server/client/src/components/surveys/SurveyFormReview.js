cd0e0bfe8c36f6c9df3ff840185490c3a0f8775f
shockliang <shockwater@gmail.com>
2019-12-18 10:58:48 +0800

    Outstanding form work.

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
 import formFields from "./formFields";
+import * as actions from "../../actions";
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -17,8 +18,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className={"yellow darken-3 btn-flat"} onClick={onCancel}>
+      <button
+        className={"yellow white-text darken-3 btn-flat"}
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        className={"green darken-3 btn-flat right white-text"}
+        onClick={() => submitSurvey(formValues)}
+      >
+        Send Survey
+        <i className={"material-icons right"}>email</i>
       </button>
     </div>
   );
@@ -30,4 +41,7 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(SurveyFormReview);
